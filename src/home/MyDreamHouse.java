3816b19a0bf0dc196cc81b4dc1ce782b40e09cec
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-05-02 22:49:05 -0700

    classs that extends abstract class to override function

---
+++ src/home/MyDreamHouse.java
@@ -0,0 +1,27 @@
+package home;
+
+public class MyDreamHouse extends HouseTemplate {
+    @Override
+    public void buildFoundation() {
+        MyUtils.log("DREAM HOUSE", "foundation build complete!");
+        
+    }
+    @Override
+    public void buildPillars() {
+        MyUtils.log("DREAM HOUSE", "pillars build complete!");
+       
+        
+    }
+    @Override
+    public void buildWalls() {
+        MyUtils.log("DREAM HOUSE", "walls build complete!");
+        
+        
+    }
+    @Override
+    public void buildWindow() {
+        MyUtils.log("DREAM HOUSE", "windows build complete!");
+        
+        
+    }
+}
