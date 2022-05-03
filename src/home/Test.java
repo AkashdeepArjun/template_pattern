e2d8bbe6eca9ca118362365f69d780338cb3bb18
AkashdeepArjun <wadhwaarjun007@gmail.com>
2022-05-02 22:49:41 -0700

    main class for testing template pattern

---
+++ src/home/Test.java
@@ -0,0 +1,10 @@
+package home;
+
+public class Test {
+    public static void main(String[] args){
+
+        MyDreamHouse dream_house= new MyDreamHouse();
+        dream_house.buildHouse();
+    }
+       
+}
